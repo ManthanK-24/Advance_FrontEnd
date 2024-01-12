d59299ca6bd4554c2b50f1511fa3bd0750210f00
ManthanK-24 <manthandec@gmail.com>
2024-01-12 20:47:07 +0530

    hooks added

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter,setCounter] = useState(0);
+
+  //let counter = 5;
+  const addValue = () =>{
+    //counter = counter + 1;
+    if(counter<=19)
+    setCounter(counter+1);
+    
+  }
+  const subValue = () => {
+    if(counter>=1)
+    {
+    setCounter(counter-1);
+    }
+  }
+
+  return (
+    <>
+      <h1>React is a Library</h1>
+      <h2>Counter value:{counter}</h2>
+      <button
+      onClick={addValue}
+      >Increase value: {counter} </button>
+      <br/>
+      <button
+      onClick={subValue}>Decrease value:{counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
