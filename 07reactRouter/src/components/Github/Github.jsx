84c4f55062731b69d5ebc720f8b65222369fd33b
ManthanK-24 <manthandec@gmail.com>
2024-08-03 08:42:13 +0530

    Some needed changes

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -16,7 +16,9 @@ function Github() {
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
         Github Followings: {data.following}
-        <img src={data.avatar_url} alt='Github picture' width={300}/>       
+        <div className='flex justify-center items-center mt-4'>
+        <img src={data.avatar_url} alt='Github picture' width={300}/>
+        </div>       
     </div>
   )
 }
